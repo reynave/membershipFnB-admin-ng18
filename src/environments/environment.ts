7be450ab14200a0f9215208e0350a0c9f5612e09
felix <felix.renaidl@gmaio.com>
2026-04-29 16:23:48 +0700

    update admin env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ declare let  apiDoc : any;
 
 export const environment = {
   production : false,
-  apiBaseUrl: 'https://nodejs.mitralinksolusi.com/api',
-  apiDoc : 'https://nodejs.mitralinksolusi.com/api-docs/'
+  apiBaseUrl: 'https://membership.mitralinksolusi.com/api',
+  apiDoc : 'https://membership.mitralinksolusi.com/api-docs/'
 };
